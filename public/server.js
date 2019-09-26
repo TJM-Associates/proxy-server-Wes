4dcf1b8d0115ef9febb07b672c8c39d4c8ac7150
Michael Kim <miketkim@umich.edu>
2019-09-26 16:08:46 -0700

    taking out other services

--- public/server.js
+++ public/server.js
@@ -17,19 +17,19 @@ app.get('/api/restaurants/:id/reviews', function (req, res) {
     res.redirect(`http://52.8.6.53:3001/api/restaurants/${req.params.id}/reviews`)
 });
 
-app.get('/api/restaurants/:id/images', function (req, res) {
-    res.redirect(`http://13.52.76.158/api/restaurants/${req.params.id}/images`)
-});
+// app.get('/api/restaurants/:id/images', function (req, res) {
+//     res.redirect(`http://13.52.76.158/api/restaurants/${req.params.id}/images`)
+// });
 
-app.get('/api/restaurants/:id/availability', function (req, res) {
-    const id = req.params.id.substring(1);
-    res.redirect(`http://54.215.234.24:3003/api/restaurants/${id}/availability?date=${req.query.date}&time=${req.query.time}&seats=${req.query.seats}`)
-});
+// app.get('/api/restaurants/:id/availability', function (req, res) {
+//     const id = req.params.id.substring(1);
+//     res.redirect(`http://54.215.234.24:3003/api/restaurants/${id}/availability?date=${req.query.date}&time=${req.query.time}&seats=${req.query.seats}`)
+// });
 
-app.get('/api/restaurants/:id/', function (req, res) {
-    const id = req.params.id.substring(1)
-    res.redirect(`http://54.215.234.24:3003/api/restaurants/${id}/`)
-});
+// app.get('/api/restaurants/:id/', function (req, res) {
+//     const id = req.params.id.substring(1)
+//     res.redirect(`http://54.215.234.24:3003/api/restaurants/${id}/`)
+// });
 
 
 
