efa8f43857c4734d3e33a3fff50c0ba34a642f61
Michael Kim <miketkim@umich.edu>
2019-09-25 17:20:07 -0700

    finished adding all 3 services

--- public/server.js
+++ public/server.js
@@ -12,45 +12,26 @@ app.use(morgan('dev'));
 
 app.use("/", express.static('loader'))
 app.use('/restaurants/:id', express.static('./public/lib'));
+// app.use('/restaurants/:restaurantId', express.static('./public/lib'))
 
 app.get('/api/restaurants/:id/reviews', function (req, res) {
-    helpers.getReviewbyID(req.body.data, function(reviewObj) {
-      // console.log(reviewObj)
-    });
-    // console.log('req data', req.body);
-  
-    res.end('got here');
-});
-
-app.get('/api/restaurants/:restaurantId/availability', function (req, res) {
-    helpers.getReservationsbyID(req.body.data, function(reservationObj) {
-      // console.log(reservationObj)
-    });
-    // console.log('req data', req.body);
-  
-    res.end('got here');
-});
-
-app.get('/api/restaurants/:restaurantId/', function (req, res) {
-    helpers.getReservationsbyID(req.body.data, function(reservationObj) {
-      console.log(reservationObj)
-    });
-    // console.log('req data', req.body);
-  
-    // res.redirect(`http://54.193.42.82:3002/api/restaurants/${req.params.rid}/images`)
+    res.redirect(`http://52.8.6.53:3001/api/restaurants/${req.params.id}/reviews`)
 });
 
 app.get('/api/restaurants/:id/images', function (req, res) {
-    console.log(req.params.id);
-    // console.log(res);
     res.redirect(`http://54.193.42.82:3002/api/restaurants/${req.params.id}/images`)
-    helpers.getPhotosbyID(req.body.data, function(photosObj) {
-      // console.log(photosObj)
-    });
-    // console.log('req data', req.body);
-    res.end('got here');
-
 });
+
+app.get('/api/restaurants/:id/availability', function (req, res) {
+    const id = req.params.id.substring(1);
+    res.redirect(`http://54.215.234.24:3003/api/restaurants/${id}/availability?date=${req.query.date}&time=${req.query.time}&seats=${req.query.seats}`)
+});
+
+app.get('/api/restaurants/:id/', function (req, res) {
+    const id = req.params.id.substring(1)
+    res.redirect(`http://54.215.234.24:3003/api/restaurants/${id}/`)
+});
+
 
 
 app.listen(port, () => console.log(`Proxy Server listening on port ${port}!`));
